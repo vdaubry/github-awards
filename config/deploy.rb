cee1e784398fb492cbd5f442f91dfbe1ff2a7bb1
Vincent Daubry <vdaubry@gmail.com>
2015-03-21 14:39:42 +0100

    clear cache after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,10 +54,9 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'cache:clear'
+      end
     end
   end
   
