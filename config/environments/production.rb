0c8fd6a73782399bb27009bc0de775040874a238
Vincent Daubry <vdaubry@gmail.com>
2015-03-25 17:18:08 +0100

    set memcache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,8 @@
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-  config.cache_store = :null_store 
+   config.cache_store = :mem_cache_store
+  #config.cache_store = :null_store 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
