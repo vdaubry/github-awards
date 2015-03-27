5b68aa70cdf1d6fa15d6e55e71df3341c9264347
Vincent Daubry <vdaubry@gmail.com>
2015-03-27 17:38:10 +0100

    disable cache for update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  caches_action :index, :search, :show, :cache_path => Proc.new { |c| c.params }
-  
   def index
     @user_list_presenter = UserListPresenter.new(params)
   end
