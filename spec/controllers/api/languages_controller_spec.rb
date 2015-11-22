bd720c76b8f48fcd48eef329645452b742598a25
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-11-22 08:43:40 +0000

    mocking languages. Removing instance variables where they's not needed

--- spec/controllers/api/languages_controller_spec.rb
+++ spec/controllers/api/languages_controller_spec.rb
@@ -6,15 +6,18 @@ describe Api::V0::LanguagesController do
     @response_body = JSON.parse(response.body)
   end
 
-
   context 'GET#index' do
-
     it 'should return a list of languages' do
+
+      mocked_languages = { 'languages' => ['JavaScript', 'Swift', 'Ruby'] }
+      JSON.stubs(:parse).returns(mocked_languages)
+
       get :index
-      expect(response_hash['languages'].count).to eq(223)
+
+      expect(response_hash['languages'].count).to eq(3)
       expect(response_hash['languages'][0]).to eq('JavaScript')
-      expect(response_hash['languages'][12]).to eq('Swift')
-      expect(response_hash['languages'][5]).to eq('Ruby')
+      expect(response_hash['languages'][1]).to eq('Swift')
+      expect(response_hash['languages'][2]).to eq('Ruby')
     end
 
   end
