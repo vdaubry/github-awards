dacf2c2b7e16ba339a69494dee9bf0be6f876f36
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-11-29 10:41:50 +0000

    Drying languages fetch code

--- app/controllers/api/v0/languages_controller.rb
+++ app/controllers/api/v0/languages_controller.rb
@@ -4,16 +4,8 @@ module Api
     class LanguagesController < ApiController
 
       def index
-        languages = read_languages_from_file
+        languages = Languages::Index.get
         respond(languages, :ok)
-      end
-
-      private
-
-      def read_languages_from_file
-        languages_file = Rails.root.join('app/assets/json/languages.json')
-        file_contents = File.read(languages_file)
-        Rails.cache.fetch('languages') { JSON.parse(file_contents) }
       end
 
     end
