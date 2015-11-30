7a7875a93a3f38319c9451e195fd7783bd802626
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-11-30 10:20:26 +0000

    Introducing Swagger-docs

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -3,6 +3,26 @@ module Api
   module V0
     class UsersController < ApiController
 
+      swagger_controller Api::V0::UsersController, 'Api::V0::UsersController'
+
+      swagger_api :index do
+        summary "Get the list of users"
+        notes "Filter users by location and language"
+
+        param :query, :language, :string, :optional, "Filter users by language"
+
+        response :ok
+      end
+
+      swagger_api :show do
+        summary "She the user details"
+        notes "Get the details of a user"
+
+        param :path, 'id', :integer, :required, 'The user\'s github login.'
+
+        response :ok
+      end
+
       def index
         user_list_presenter = UserListPresenter.new(params)
         user_ranks = user_list_presenter.user_ranks
