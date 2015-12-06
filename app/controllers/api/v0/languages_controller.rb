2946060d8cfbc904785bbc2c396c2a01856464a3
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-12-06 18:47:43 +0000

    Languages documentation

--- app/controllers/api/v0/languages_controller.rb
+++ app/controllers/api/v0/languages_controller.rb
@@ -3,6 +3,17 @@ module Api
   module V0
     class LanguagesController < ApiController
 
+      swagger_controller Api::V0::LanguagesController, 'Api::V0::LanguagesController'
+
+      swagger_api :index do
+        summary "Get the list of languages."
+        notes "You can choose between an alphabetical order or by popularity."
+
+        param :query, :sort, :string, :optional, "Sort order. (alphabetical or popularity)"
+
+        response :ok
+      end
+
       def index
         sort = params["sort"].try(:to_sym) || :alphabetical
         languages = Languages::Index.get(sort: sort)
